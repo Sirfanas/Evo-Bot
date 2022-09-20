f8b37acf727b5b3a822fecbc16e96d0b0d85f13e
Sirfanas <romain.fauquet59@gmail.com>
2022-09-20 18:20:35 +0200

    Unified code and add small 'doc'

--- evobot/models/mob.py
+++ evobot/models/mob.py
@@ -6,12 +6,12 @@ from . import Entity
 class Mob(Entity):
     mob_type = 'mob'  # Define the mob type, you can choose them
 
-    def move(self, mob_id: int, direction: str) -> bool:
+    def move(self, oid: int, direction: str) -> bool:
         """
           Move the mob if possible. If the move was successfull return True
                                     Else False
 
-          :param int mov_id: Mob id
+          :param int oid: Mob id
           :param str direction: One of {
             'NW', # North West
             'N',  # North             NW  N  NE
--- evobot/storage/cache.py
+++ evobot/storage/cache.py
@@ -3,17 +3,22 @@
 class Cache:
     """
         Used to quickly store data, will be bridged with a real database.
+
+        create(key: str, datas: dict) -> int
+        read(key: str, oid: int) -> dict
+        update(key: str, oid: int, datas: dict)
+        delete(key: str, oid: int)
     """
 
     def __init__(self):
         self._datas = dict()
         self._sequences = dict()
 
-    def create(self, key, datas: dict) -> int:
+    def create(self, key: str, datas: dict) -> int:
         """
             Store datas in specified keys
 
-            :param key: Any serializable value (str, int...)
+            :param str key: Key to store datas (kind of "table" name)
             :param dict datas: Datas to store as dict
             :return created id
             :rtype: int
@@ -24,11 +29,11 @@ class Cache:
         self._datas[key][new_id] = datas
         return new_id
 
-    def read(self, key, oid: int):
+    def read(self, key: str, oid: int) -> dict:
         """
             Read datas of specified oid of keys
 
-            :param key: Any serializable value (str, int...)
+            :param str key: Key to store datas (kind of "table" name)
             :param int oid: associated id
             :return value of key/id
         """
@@ -36,32 +41,32 @@ class Cache:
         oid_datas = key_data[oid]  # If crashed here -> oid doesn't exists
         return oid_datas
 
-    def update(self, key, oid: int, value: dict):
+    def update(self, key: str, oid: int, datas: dict):
         """
             Update data of key/oid with value
 
-            :param key: Any serializable value (str, int...)
+            :param str key: Key to store datas (kind of "table" name)
             :param int oid: associated id
-            :param dict value: Value to update
+            :param dict datas: Value to update
         """
-        self._datas[key][oid].update(value)
+        self._datas[key][oid].update(datas)
 
-    def delete(self, key, oid: int):
+    def delete(self, key: str, oid: int):
         """
             Delete record of key/id, can't be restored !!
 
-            :param key: Any serializable value (str, int...)
+            :param str key: Key to store datas (kind of "table" name)
             :param int oid: associated id
         """
         del self._datas[key][oid]
 
-    def _next_id(self, key) -> int:
+    def _next_id(self, key: str) -> int:
         """
             Return the next id based on _sequences
             if not set then initialize at 1
             automaticly increment it.
 
-            :param key: Any serializable value (str, int...)
+            :param str key: Key to store datas (kind of "table" name)
             :return generated id
             :rtype: int
         """
@@ -69,3 +74,6 @@ class Cache:
         next_id = current_id + 1
         self._sequences[key] = next_id
         return next_id
+
+
+Cache = Cache()
